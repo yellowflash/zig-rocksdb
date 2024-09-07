a92551940ae7661b762261be0fcc15d76f1c1705
Yellowflash <minato.yelloflash@gmail.com>
2024-09-07 11:21:46 +0530

    Exporting modules

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const version = @import("builtin").zig_version;
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const test_step = b.step("test", "Run unit tests");
@@ -12,6 +12,11 @@ pub fn build(b: *std.Build) void {
     const unit_tests = b.addTest(.{ .root_source_file = b.path("rocksdb.zig"), .target = target, .optimize = optimize });
     unit_tests.linkLibC();
 
+    const module = b.createModule(.{
+        .root_source_file = b.path("rocksdb.zig"),
+    });
+
+    try b.modules.put(b.dupe("rocksdb"), module);
     test_step.dependOn(&b.addRunArtifact(unit_tests).step);
 
     zigRocksDb.linkSystemLibrary("rocksdb");
