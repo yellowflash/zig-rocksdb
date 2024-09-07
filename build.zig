afad2398d9041a68d80221475f029d256114cec8
Yellowflash <minato.yelloflash@gmail.com>
2024-09-07 10:49:20 +0530

    Using LazyPath api to build path

--- build.zig
+++ build.zig
@@ -6,10 +6,10 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const test_step = b.step("test", "Run unit tests");
 
-    const zigRocksDb = b.addStaticLibrary(.{ .name = "rocksdb", .root_source_file = .{ .path = "rocksdb.zig" }, .target = target, .optimize = optimize });
+    const zigRocksDb = b.addStaticLibrary(.{ .name = "rocksdb", .root_source_file = b.path("rocksdb.zig"), .target = target, .optimize = optimize });
     zigRocksDb.linkLibC();
 
-    const unit_tests = b.addTest(.{ .root_source_file = .{ .path = b.pathFromRoot("rocksdb.zig") }, .target = target, .optimize = optimize });
+    const unit_tests = b.addTest(.{ .root_source_file = b.path("rocksdb.zig"), .target = target, .optimize = optimize });
     unit_tests.linkLibC();
 
     test_step.dependOn(&b.addRunArtifact(unit_tests).step);
